8afcdd683b2f3ec02a708336f338d4c97b65d9ab
Duc.NA <ducnafs1579@gmail.com>
2025-03-12 13:15:02 +0700

    backup code

---
+++ StartTrack/StartTrack/Screen/Splash/FusionSplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FusionSplashViewController.swift
+//  StartTrack
+//
+//  Created by Hartzed Story on 12/3/25.
+//
+
+import UIKit
+
+class FusionSplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
